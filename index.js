f9b0d50cf9526880f5ee57039ed26c3545a1dc26
Junior Alves <jrnalves@gmail.com>
2021-09-23 09:32:52 -0300

    refactory rename variable

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const todoList = ["Assistir Breakin Bad", "Planejar aulas", "Fazer exercícios f
 
 function addTodoListInHTML(array) {
     const htmlList = array.map(item => (
-            `<li class="list-group-item">
-                <span>${item}</span>
-                <i class="far fa-trash-alt delete" data-trash="delete"></i>
-            </li>`
-        )
+        `<li class="list-group-item">
+            <span>${item}</span>
+            <i class="far fa-trash-alt delete" data-trash="delete"></i>
+        </li>`
     )
-    
+    )
+
     $todosContainer.innerHTML = htmlList.join(' ')
 }
 
-const addItemToTodoList =  event => {
+const addItemToTodoList = event => {
     event.preventDefault()
     const todo = event.target.add.value.trim()
-    
+
     todoList.push(todo)
     addTodoListInHTML(todoList)
 
@@ -30,23 +30,22 @@ function removeItemToTodoList(event) {
     const eventTarget = event.target
 
     if (eventTarget.dataset.trash) {
-        
+
         const todo = eventTarget.previousElementSibling.innerHTML
 
         const removeIndex = todoList.indexOf(todo)
         todoList.splice(removeIndex, 1)
-        
+
         addTodoListInHTML(todoList)
     }
 }
 
 const searchItemInTodoList = event => {
     event.preventDefault()
-    const searchWord = $formSearch.search.value.toLowerCase()
+    const searchTerm = $formSearch.search.value.toLowerCase()
 
-    const listWordMatch = todoList.filter(item => item.toLowerCase().includes(searchWord))
+    const listWordMatch = todoList.filter(item => item.toLowerCase().includes(searchTerm))
     addTodoListInHTML(listWordMatch)
-
 }
 
 $formAddTodo.addEventListener('submit', addItemToTodoList)
